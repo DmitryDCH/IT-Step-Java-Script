6aa769f4e98e09a0c1edce67025f006113dd1cc8
Dima Chekurkov <Chekurkov@gmail.com>
2019-02-21 22:57:19 +0200

    First home-work created

---
+++ First Home-Work (simple calculator)/js/main.js
@@ -0,0 +1,6 @@
+var first_number = prompt("Enter first number","");
+var second_number = prompt("Enter second number", "");
+
+var result = +first_number + +second_number;
+
+document.write("<strong> Result of add first and second number is : </strong>" + result);
