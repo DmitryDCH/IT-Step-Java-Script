fea508330c35144e4741f881ded71111ad4e71e9
Dmitry Chekurkov <chekurkov@gmail.com>
2019-03-06 16:47:44 +0200

    working version

--- while_park/js/main.js
+++ while_park/js/main.js
@@ -1,3 +1,4 @@
+/*
 function money_data(){
     var money_value = document.querySelector(".money_value").value;
     //var atraction_value = document.querySelector(".atraction_value").value;
@@ -9,30 +10,52 @@ function money_data(){
     if (money <= 0){
         console.log("R.T.F.M");
     }  else{
-        while(money > 0){
-          var atraction_value =  prompt("Enter value of atraction");
-          var atraction = +atraction_value;
-          if( atraction <= 0 || atraction > 6){
-               alert("Enter valid number of atraction")
-          }else{
+          while(money > 0){
+               var atraction_value =  prompt("Enter value of atraction", " ");
+               var atraction = +atraction_value;
+          if( atraction <= 0 || atraction > 6 ){
+               alert("Enter valid number of atraction");
+          } else if (atraction == null){
+               console.log("works");
+          } else {
                switch(atraction){
                     case 1:
-                         money-=100;
+                         if (money < 100){
+                              alert("Need more money");
+                         } else{
+                              money -=100;
+                         }
                     break;
                     case 2:
-                         money -=200;
+                         if (money < 200){
+                              alert("Need more money");
+                         } else{
+                              money -=200;
+                         }
                     break;
                     case 3:
-                         money -=50;
+                         if (money < 50){
+                              alert("Need more money");
+                         } else{
+                              money -=50;
+                         }
                     break;
                     case 4:
-                         money -=300;
+                         if (money < 300){
+                              alert("Need more money");
+                         } else{
+                              money -=300;
+                         }
                     break;
                     case 5:
-                         money -=250;
+                         if (money < 250){
+                              alert("Need more money");
+                         } else{
+                              money -=250;
+                         }
                     break;
                     default:
-                     console.log("How you do this ?");
+                         console.log("How you do this ?");
                     break;
                }
           }
@@ -41,3 +64,73 @@ function money_data(){
     }
     alert("You money is 0");
 }
+
+*/
+
+function money_data(){
+     var money_value = document.querySelector(".money_value").value;
+     //var atraction_value = document.querySelector(".atraction_value").value;
+ 
+     var money = +money_value;
+     //console.log(typeof(money));
+     //console.log(typeof(atraction));
+ 
+     if (money <= 0){
+         console.log("R.T.F.M");
+     }else{
+          while(money > 0){
+             var atraction_value = prompt("Enter value of atraction", '');
+               if (atraction_value === null) {
+                  alert("cancel");
+               return;
+             }
+             var atraction = +atraction_value;
+               if( atraction < 0 || atraction > 6){
+                 alert("Enter valid number of atraction")
+               } else {
+               switch(atraction){
+                    case 1:
+                         if (money < 100){
+                              alert("Need more money");
+                         } else{
+                              money -=100;
+                         }
+                    break;
+                    case 2:
+                         if (money < 200){
+                              alert("Need more money");
+                         } else{
+                              money -=200;
+                         }
+                    break;
+                    case 3:
+                         if (money < 50){
+                              alert("Need more money");
+                         } else{
+                              money -=50;
+                         }
+                    break;
+                    case 4:
+                         if (money < 300){
+                              alert("Need more money");
+                         } else{
+                              money -=300;
+                         }
+                    break;
+                    case 5:
+                         if (money < 250){
+                              alert("Need more money");
+                         } else{
+                              money -=250;
+                         }
+                    break;
+                    default:
+                         console.log("How you do this ?");
+                    break;
+               }
+             }
+             alert("Now you have a " + money + "$");
+         }
+     }
+     alert("You money is 0");
+ }
