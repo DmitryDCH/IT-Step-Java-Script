5fb88edd77cd0a05a5c4a80b8e3dd3d7333071b0
Dmitry Chekurkov <chekurkov@gmail.com>
2019-03-06 12:10:26 +0200

    while

---
+++ Sixth Work -while (while)/js/main.js
@@ -0,0 +1,53 @@
+/* first task 
+
+var number = 0;
+var sum = 0;
+while(number < 10){
+    number++;
+    //console.log(number);
+    sum += number;
+    //console.log(number, sum);
+}
+console.log(sum / 10);
+
+*/
+
+/* second task
+
+
+var i = 0;
+
+do {
+    i++;
+    console.log(i);
+} while (i < 100);
+
+*/
+
+
+/* third task
+
+var i = 100;
+
+do {
+    console.log(i);
+    i--;
+} while (i >= 1);
+
+
+*/
+
+/*
+fourth task
+
+var i = 0;
+
+while(i < 100){
+    i++;
+    if (i % 2 == 1){
+        console.log(i);
+    }
+    
+}
+
+*/
